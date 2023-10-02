3984a48eed69e1599ed577876b3a1dd07db73887
Xavier Hayes <xavier.hayes@southeasttech.edu>
2023-10-01 22:51:09 -0500

    Added a Genre class to the Models folder. Then, modifed the class  so it contains the code shown in figure 4-14.

---
+++ MovieList/Models/Genre.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieList.Models
+{
+    public class Genre
+    {
+
+        public string GenreId { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+
+    }
+}
