586c894c4605fd0f01c109ae68b1fefff40edb75
Xavier Hayes <xavier.hayes@southeasttech.edu>
2023-10-01 21:58:24 -0500

    12.Added a Migrations folder and migration files to the Solution Explorer.

--- MovieList/Program.cs
+++ MovieList/Program.cs
@@ -1,7 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using MovieList.Models;
+
+
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
+builder.Services.AddDbContext<MovieContext>(options =>
+options.UseSqlServer(
+builder.Configuration.GetConnectionString("MovieContext")));
 
 var app = builder.Build();
 
