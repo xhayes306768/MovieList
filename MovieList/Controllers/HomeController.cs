8bc2d5bc091cef85d2dd2aee11299e764ea9059b
Xavier Hayes <xavier.hayes@southeasttech.edu>
2023-10-01 22:37:13 -0500

    Modifed the Home controller so it contains the code shown in figure 4-10.

--- MovieList/Controllers/HomeController.cs
+++ MovieList/Controllers/HomeController.cs
@@ -1,13 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MovieList.Models;
-
 namespace MovieList.Controllers
 {
     public class HomeController : Controller
     {
+        private MovieContext context { get; set; }
+        public HomeController(MovieContext ctx) => context = ctx;
         public IActionResult Index()
         {
-            return View();
+            var movies = context.Movies.OrderBy(m => m.Name).ToList(); return View(movies);
         }
     }
 }
+
